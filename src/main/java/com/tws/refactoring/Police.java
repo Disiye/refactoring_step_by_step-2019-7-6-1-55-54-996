5efc7d3c0476aa33069b2d9dd44af81bd38fec2b
WENGEM <emilio.wang@oocl.com>
2019-07-15 14:07:31 +0800

    Refactor the code

--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -2,7 +2,9 @@ package com.tws.refactoring;
 
 public class Police {
     public boolean checkDriver(Driver driver) {
-        if(driver.age >= 18) return true;
-        else return false;
+        int maxAge = 18;
+        return driver.getAge() >= maxAge;
+//        if(driver.age >= 18) return true;
+//        else return false;
     }
 }
